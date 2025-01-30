48a46450ff772d4d0a112d8063c8a59d209be535
yutongliang723 <susanliang723@gmail.com>
2025-01-30 21:20:13 +0100

    new file:   run_results/30-01-2025&21-16-47/parameters.json 	new file:   run_results/30-01-2025&21-16-47/simulation.gif 	new file:   run_results/30-01-2025&21-16-47/simulation_plots.svg 	modified:   running.ipynb 	modified:   test.ipynb 	modified:   variables.py

--- variables.py
+++ variables.py
@@ -45,12 +45,14 @@ other_para = ['rho', 'pstar', 'mortparms']
 bins = pd.cut(vec1['age'], bins=new_max_age)
 binned_vec = pd.DataFrame()
 for col in other_para:
-    binned_col = vec1.groupby(bins).agg({col: 'mean'}).reset_index()
+    binned_col = vec1.groupby(bins, observed=False).agg({col: 'mean'}).reset_index()
     binned_col[col] = binned_col[col] * scale_factor
     binned_vec[col] = binned_col[col]
 
 bin_centers = [interval.mid for interval in binned_col['age']]
 binned_vec = binned_vec.rename_axis('age_new').reset_index()
+binned_vec.loc[binned_vec['age_new'] <= 3, 'pstar'] *= 0.5
+
 binned_vec['mstar'] = vec1['mstar']
 binned_vec['fertparm'] = vec1['fertparm']
 binned_vec['mortscale'] = vec1['mortscale']
