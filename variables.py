b939d4899f6d8569510c3f0036815017cd123e0f
yutongliang723 <susanliang723@gmail.com>
2025-01-30 22:18:20 +0100

    modified:   parameters.json 	new file:   run_results/30-01-2025&22-16-34/parameters.json 	new file:   run_results/30-01-2025&22-16-34/simulation.gif 	new file:   run_results/30-01-2025&22-16-34/simulation_plots.svg 	new file:   run_results/30-01-2025&22-16-34/vec1.txt 	modified:   running.ipynb 	modified:   variables.py

--- variables.py
+++ variables.py
@@ -36,28 +36,28 @@ file_path = params["file_path"]
 file_name = params["file_name"]
 
 vec1 = pd.read_csv('demog_vectors.csv')
-vec1 = vec1.rename_axis('age').reset_index()
-new_max_age = 60
-old_max_age = vec1['age'].max()
-scale_factor = new_max_age / old_max_age
-scale_factor = 1
-other_para = ['rho', 'pstar', 'mortparms']
-bins = pd.cut(vec1['age'], bins=new_max_age)
-binned_vec = pd.DataFrame()
-for col in other_para:
-    binned_col = vec1.groupby(bins, observed=False).agg({col: 'mean'}).reset_index()
-    binned_col[col] = binned_col[col] * scale_factor
-    binned_vec[col] = binned_col[col]
+# vec1 = vec1.rename_axis('age').reset_index()
+# new_max_age = 60
+# old_max_age = vec1['age'].max()
+# scale_factor = new_max_age / old_max_age
+# scale_factor = 1
+# other_para = ['rho', 'pstar', 'mortparms']
+# bins = pd.cut(vec1['age'], bins=new_max_age)
+# binned_vec = pd.DataFrame()
+# for col in other_para:
+#     binned_col = vec1.groupby(bins, observed=False).agg({col: 'mean'}).reset_index()
+#     binned_col[col] = binned_col[col] * scale_factor
+#     binned_vec[col] = binned_col[col]
 
-bin_centers = [interval.mid for interval in binned_col['age']]
-binned_vec = binned_vec.rename_axis('age_new').reset_index()
-binned_vec.loc[binned_vec['age_new'] <= 3, 'pstar'] *= 0.5
+# bin_centers = [interval.mid for interval in binned_col['age']]
+# binned_vec = binned_vec.rename_axis('age_new').reset_index()
+# binned_vec.loc[binned_vec['age_new'] <= 3, 'pstar'] *= 0.5
 
-binned_vec['mstar'] = vec1['mstar']
-binned_vec['fertparm'] = vec1['fertparm']
-binned_vec['mortscale'] = vec1['mortscale']
-binned_vec['fertscale'] = vec1['fertscale']
-binned_vec['phi'] = vec1['phi']
-vec1 = binned_vec
+# binned_vec['mstar'] = vec1['mstar']
+# binned_vec['fertparm'] = vec1['fertparm']
+# binned_vec['mortscale'] = vec1['mortscale']
+# binned_vec['fertscale'] = vec1['fertscale']
+# binned_vec['phi'] = vec1['phi']
+# vec1 = binned_vec
 
 
