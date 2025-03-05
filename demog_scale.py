eb64b76e45024a02dd34c4dc5d9ba4fb6d129148
yutongliang723 <susanliang723@gmail.com>
2025-03-05 23:46:53 +0100

    modified:   demog_scale.py 	modified:   demog_vectors_scaled.csv 	modified:   parameters.json 	modified:   test.ipynb

--- demog_scale.py
+++ demog_scale.py
@@ -13,7 +13,7 @@ def demog_scale():
 
     # Store original values
     original_age = vec1_copy['age']
-    original_values = {param: vec1_copy[param] for param in list(vec1_copy.columns)}
+    original_values = {param: vec1_copy[param] for param in ['fertparm', 'mortscale', 'fertscale', 'mstar', 'phi', 'm0', 'pstar', 'p0']}
 
     # Shrink only the x-axis (age), keeping y-values the same
     new_ages = original_age * scale_factor
@@ -23,5 +23,10 @@ def demog_scale():
     for param in original_values.keys():
         rescaled_df[param] = original_values[param]  # Keep y-values unchanged
 
+    df_rebinned = rescaled_df[['pstar', 'p0']].groupby(rescaled_df.index // 2).mean().reset_index(drop=True)
+    df_other_para = vec1_copy.loc[:, ~vec1_copy.columns.isin(['pstar', 'p0'])][:60]
+    df_merged = pd.concat([df_rebinned, df_other_para], axis=1)
+
+
     # vec1_copy.to_csv('demog_vectors_scaled.csv')
-    rescaled_df.to_csv('demog_vectors_scaled.csv')
+    df_merged.to_csv('demog_vectors_scaled.csv')
